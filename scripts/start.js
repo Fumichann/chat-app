d206bee06adb796fe0c9f916dab8bcc810a44ea0
morimoringo <morimoringo@example.com>
2025-06-29 21:51:23 +0900

    start.jsを追加

---
+++ scripts/start.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startScreen = document.getElementById("start-screen");
+
+  // 表示
+  startScreen.style.display = "flex";
+
+  // クリックで index.html に遷移
+  document.addEventListener("click", () => {
+    window.location.href = "index.html";
+  }, { once: true });
+});
